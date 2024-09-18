0cb8bede091ce10216c2f6d09747c209494c7a0a
rj-d111 <raymondaguillar1@gmail.com>
2024-09-18 09:53:24 +0800

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import HomeGuest from "./pages/HomeGuest";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
-    <h1 className="text-2xl bg-red-500">Hello World</h1>
-    </>
+  <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  </>
   );
 }
 
---
+++ src/pages/HomeGuest.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomeGuest() {
+  return (
+    <div>
+      Home Guest Page
+    </div>
+  )
+}
