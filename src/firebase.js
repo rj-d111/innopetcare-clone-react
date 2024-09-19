7f5c18ba6be1a5847976d88e15e744b7a1de7e69
rj-d111 <raymondaguillar1@gmail.com>
2024-09-19 16:46:10 +0800

    Register Firebase Working

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDNWx6D1QyaezRE9TM8C-b6_gbxJXepLWU",
+  authDomain: "innopetcare-2a866.firebaseapp.com",
+  databaseURL: "https://innopetcare-2a866-default-rtdb.firebaseio.com",
+  projectId: "innopetcare-2a866",
+  storageBucket: "innopetcare-2a866.appspot.com",
+  messagingSenderId: "485214746152",
+  appId: "1:485214746152:web:872da38df2d3c8df82e71d",
+  measurementId: "G-SCXNZK8VMZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,22 +1,104 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import OAuth from "../components/OAuth";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { db } from "../firebase";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
+    name: "",
     email: "",
     password: "",
+    confirm_password: "",
+    phone: "",
   });
+  const [error, setError] = useState("");
+
   const { name, phone, email, password, confirm_password } = formData;
+  const navigate = useNavigate();
+
+
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+    setError(""); // Clear error on change
   }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    // Validation
+    if (!name || !email || !password || !confirm_password || !phone) {
+      setError("Please fill the required fields");
+      return;
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      setError("Need to provide a valid email");
+      return;
+    }
+
+    if (password.length < 8) {
+      setError("Password must be at least 8 characters long");
+      setFormData((prevState) => ({
+        ...prevState,
+        password: "",
+        confirm_password: "",
+      }));
+      return;
+    }
+
+    if (password !== confirm_password) {
+      setError("The password does not match with confirm password");
+      setFormData((prevState) => ({
+        ...prevState,
+        password: "",
+        confirm_password: "",
+      }));
+      return;
+    }
+
+    if (!/^9\d{9}$/.test(phone)) {
+      setError("The phone number must start with 9 and be followed by 9 digits.");
+      return;
+    }
+    
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+      const user = userCredential.user;
+      console.log(user);
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      delete formDataCopy.confirm_password;
+      formDataCopy.phone = "+63" + formDataCopy.phone;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+      navigate("/");
+    } catch (error) {
+
+
+      if (error.code === "auth/email-already-in-use") {
+        setError("Email address already in use. Please sign in.");
+      }
+      else{
+        setError(error.message || "An unexpected error occurred.");
+      }
+      console.log(error);
+    }
+  }
+
   return (
     <section className="bg-gray-100 min-h-screen flex items-center justify-center">
       <div className="container flex flex-col md:flex-row items-center justify-center my-10 mx-3">
@@ -27,31 +109,32 @@ export default function Register() {
               alt="InnoPetCare Logo"
               className="mx-auto mb-4"
             />
-            <h2 class="font-bold text-yellow-900 flex flex-col sm:flex-row items-center justify-center">
+            <h2 className="font-bold text-yellow-900 flex flex-col sm:flex-row items-center justify-center">
               Welcome!
-              <span class="sm:ml-1">Login to your account.</span>
+              <span className="sm:ml-1">Login to your account.</span>
             </h2>
             <p className="text-sm text-gray-600 mt-1">
               Already Registered?{" "}
-              <span
-                className="text-sm text-yellow-600 hover:underline hover:text-yellow-800 transition duration-200 ease-in-out"
-              >
-                <Link to="/login">
-                Login
-                </Link>
+              <span className="text-sm text-yellow-600 hover:underline hover:text-yellow-800 transition duration-200 ease-in-out">
+                <Link to="/login">Login</Link>
               </span>
             </p>
           </div>
 
-          <form action="LoginController" method="POST">
+          {error && (
+            <div className="bg-red-500 text-white p-4 rounded-lg mb-4">
+              {error}
+            </div>
+          )}
+
+          <form onSubmit={onSubmit}>
             <div className="mb-5">
               <label htmlFor="name" className="block text-gray-500 mb-2">
                 Name
               </label>
               <input
-                type="name"
+                type="text"
                 id="name"
-                name="name"
                 value={name}
                 onChange={onChange}
                 placeholder="Enter your name"
@@ -65,30 +148,27 @@ export default function Register() {
               <input
                 type="email"
                 id="email"
-                name="email"
                 value={email}
                 onChange={onChange}
                 placeholder="Enter your email"
                 className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"
               />
             </div>
-            <div class="mb-5">
-              <label for="contact" class="block text-gray-500 mb-2">
+            <div className="mb-5">
+              <label htmlFor="phone" className="block text-gray-500 mb-2">
                 Phone No. (Format: +639XXXXXXXXX)
               </label>
-              <div class="flex">
-                <div class="w-1/6 bg-gray-200 text-center py-2 px-3 rounded-l-lg border border-gray-300">
+              <div className="flex">
+                <div className="w-1/6 bg-gray-200 text-center py-2 rounded-l-lg border border-gray-300">
                   +63
                 </div>
                 <input
                   type="tel"
                   id="phone"
-                  name="phone"
                   value={phone}
                   onChange={onChange}
                   placeholder="Enter your Phone No."
-                  class="w-5/6 px-4 py-2 rounded-r-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"
-                  pattern="9[0-9]{9}"
+                  className="w-5/6 px-4 py-2 rounded-r-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"
                 />
               </div>
             </div>
@@ -99,14 +179,12 @@ export default function Register() {
               </label>
               <div className="relative">
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   id="password"
-                  name="password"
                   value={password}
                   onChange={onChange}
                   placeholder="Enter your password"
                   className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"
-                
                 />
                 {showPassword ? (
                   <FaEyeSlash
@@ -122,19 +200,18 @@ export default function Register() {
               </div>
             </div>
             <div className="mb-5">
-              <label htmlFor="password" className="block text-gray-500 mb-2">
+              <label htmlFor="confirm_password" className="block text-gray-500 mb-2">
                 Confirm Password
               </label>
-                <input
-                  type="password"
-                  id="confirm_password"
-                  name="confirm_password"
-                  value={confirm_password}
-                  onChange={onChange}
-                  placeholder="Confirm your password"
-                  className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"
-                />
-              </div>
+              <input
+                type="password"
+                id="confirm_password"
+                value={confirm_password}
+                onChange={onChange}
+                placeholder="Confirm your password"
+                className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"
+              />
+            </div>
             <button
               type="submit"
               className="w-full uppercase bg-yellow-600 hover:bg-yellow-700 text-white py-3 rounded-lg font-semibold shadow-md hover:shadow-lg active:shadow-lg transition duration-200 ease-in-out"
